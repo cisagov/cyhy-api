03bd763c5706260808cb6575dfc3fe4c74e00097
Felddy <markf+github@geekpad.com>
2019-02-13 00:41:32 -0500

    Flake8 it

--- src/cyhy_rest/api.py
+++ src/cyhy_rest/api.py
@@ -1,13 +1,17 @@
+"""CyHy REST API Server."""
+
 from flask import Flask, g, jsonify
 from flask_restful import reqparse, abort, Api, Resource
 from flask_httpauth import HTTPBasicAuth
 from itsdangerous import (TimedJSONWebSignatureSerializer
                           as Serializer, BadSignature, SignatureExpired)
 
-
 app = Flask(__name__)
-api = Api(app)
 auth = HTTPBasicAuth()
+api = Api(app)
+parser = reqparse.RequestParser()
+parser.add_argument('task')
+
 
 # Set the secret key to some random bytes. Keep this really secret!
 # TODO get from secret yml
@@ -20,28 +24,25 @@ TODOS = {
 }
 
 
-def abort_if_todo_doesnt_exist(todo_id):
-    if todo_id not in TODOS:
-        abort(404, message="Todo {} doesn't exist".format(todo_id))
-
-
-parser = reqparse.RequestParser()
-parser.add_argument('task')
-
-
 class User():
+    """A simple user object."""
+
     def __init__(self, id):
+        """Create a user object."""
         self.id = id
 
     def verify_password(self, password):
+        """Verify the user's password."""
         return password == 'foo'
 
     def generate_auth_token(self, expiration=600):
+        """Generate a secure authentication token."""
         s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
         return s.dumps({'id': self.id})
 
     @staticmethod
     def verify_auth_token(token):
+        """Verify a token is valid, and return the associate username."""
         s = Serializer(app.config['SECRET_KEY'])
         try:
             data = s.loads(token)
@@ -55,6 +56,7 @@ class User():
 
 @auth.verify_password
 def verify_password(username_or_token, password):
+    """Verify a username:password or token."""
     # first try to authenticate by token
     user = User.verify_auth_token(username_or_token)
     if not user:
@@ -67,27 +69,37 @@ def verify_password(username_or_token, password):
 
 
 class Token(Resource):
+    """Allow a user to request a token for use instead of username:password."""
+
     @auth.login_required
     def get(self):
+        """Return a new token for an authenticated user."""
         token = g.user.generate_auth_token(600)
         return jsonify({'token': token.decode('ascii'), 'duration': 600})
 
 
-# Todo
-# shows a single todo item and lets you delete a todo item
 class Todo(Resource):
+    """Handle a single Todo items."""
+
+    def _abort_if_todo_doesnt_exist(self, todo_id):
+        if todo_id not in TODOS:
+            abort(404, message="Todo {} doesn't exist".format(todo_id))
+
     def get(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
+        """Return a ToDo by id."""
+        self._abort_if_todo_doesnt_exist(todo_id)
         return TODOS[todo_id]
 
     @auth.login_required
     def delete(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
+        """Delete a ToDo by id."""
+        self._abort_if_todo_doesnt_exist(todo_id)
         del TODOS[todo_id]
         return '', 204
 
     @auth.login_required
     def put(self, todo_id):
+        """Create or update a ToDo."""
         args = parser.parse_args()
         task = {'task': args['task']}
         TODOS[todo_id] = task
@@ -97,10 +109,15 @@ class Todo(Resource):
 # TodoList
 # shows a list of all todos, and lets you POST to add new tasks
 class TodoList(Resource):
+    """Handle lists of Todo items."""
+
     def get(self):
+        """Return the list of all Todos."""
         return TODOS
 
+    @auth.login_required
     def post(self):
+        """Add multiple Todo items at once."""
         args = parser.parse_args()
         todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
         todo_id = 'todo%i' % todo_id
@@ -108,15 +125,14 @@ class TodoList(Resource):
         return TODOS[todo_id], 201
 
 
-##
-# Actually setup the Api resource routing here
-##
+# Add routes to the API resources
 api.add_resource(TodoList, '/todos')
 api.add_resource(Todo, '/todos/<todo_id>')
 api.add_resource(Token, '/token')
 
 
 def main():
+    """Start the application."""
     app.run(host='0.0.0.0', debug=True)
 
 
