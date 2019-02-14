5e2b8c8cb1c5c1bf7715318d76ea96b9f03db390
Felddy <markf+github@geekpad.com>
2019-02-14 00:07:04 -0500

    Add test application user to db init

---
+++ tests/init/mongo-init.js
@@ -0,0 +1,16 @@
+db = db.getSiblingDB('cyhy')
+
+db.createUser({
+  user: "rest-server",
+  pwd: "example",
+  roles: [{
+    role: "readWrite",
+    db: "cyhy"
+  }]
+});
+
+// password: foobar
+db.users.insert({
+  "_id": "felddy",
+  "hash": "$2b$12$vsQwMGt6iHQY.wBqQpWDNuVf6EjXwPH28OvYtQ2.6iKuWYw2tMLLq"
+})
