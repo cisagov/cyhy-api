f74108e0329c671e5324faa121b30945616e704a
Felddy <markf+github@geekpad.com>
2019-02-14 00:06:19 -0500

    Create a User model

---
+++ src/cyhy_rest/models.py
@@ -0,0 +1,45 @@
+from pymodm import MongoModel, fields
+from pymongo.write_concern import WriteConcern
+import bcrypt
+from flask import current_app as app
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
+
+
+class User(MongoModel):
+    username = fields.CharField(primary_key=True)
+    hash = fields.CharField()
+
+    class Meta:
+        indexes = []
+        write_concern = WriteConcern(j=True)
+        collection_name = 'users'
+        final = True
+
+    def check_password(self, password):
+        return bcrypt.checkpw(password.encode('utf-8'),
+                              self.hash.encode('utf-8'))
+
+    def hash_password(self, password):
+        salt = bcrypt.gensalt()
+        hash_bytes = bcrypt.hashpw(password.encode('utf-8'), salt)
+        self.hash = hash_bytes.decode()
+
+    def generate_auth_token(self, expiration=600):
+        """Generate a secure authentication token."""
+        s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'id': self.username})
+
+    @staticmethod
+    def verify_auth_token(token):
+        """Verify a token is valid, and return the associate username."""
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None    # valid token, but expired
+        except BadSignature:
+            return None    # invalid token
+        query = User.objects.raw({'_id': data['id']})
+        user = query[0]  # TODO: move this to a custom manager
+        return user
