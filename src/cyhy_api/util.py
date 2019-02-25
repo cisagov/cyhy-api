e2448f2075b89fa821111b066a627b615a34476f
Felddy <markf+github@geekpad.com>
2019-02-25 12:42:24 -0500

    Rename secret targets

--- src/cyhy_api/util.py
+++ src/cyhy_api/util.py
@@ -4,7 +4,7 @@ import yaml
 from mongoengine import connect
 
 
-def load_config(filename='/run/secrets/config_yml'):
+def load_config(filename='/run/secrets/config.yml'):
     """Load a configuration file."""
     print(f'Reading configuration from {filename}')
     with open(filename, 'r') as stream:
