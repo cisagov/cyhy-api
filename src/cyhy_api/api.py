28d17e5efd4c411f22d055ad603292dcd7ba2711
Felddy <markf+github@geekpad.com>
2019-02-25 15:51:51 -0500

    Update API to use GraphQL. Not fully baked.

--- src/cyhy_api/api.py
+++ src/cyhy_api/api.py
@@ -1,116 +1,89 @@
 """CyHy API Server."""
 
-from flask import Flask, g, jsonify
-from flask_restful import reqparse, abort, Api, Resource
-from flask_httpauth import HTTPBasicAuth
-from mongoengine.errors import DoesNotExist
+from flask import Flask, g, jsonify, request
+from flask_graphql import GraphQLView
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
 
 from .models import User
 from .util import connect_from_config
+from .schema import schema
 
 app = Flask(__name__)
-auth = HTTPBasicAuth()
-api = Api(app)
-parser = reqparse.RequestParser()
-parser.add_argument('task')
+app.config['DEBUG'] = True
+jwt = JWTManager(app)
 
 
-# Set the secret key to some random bytes. Keep this really secret!
-# TODO get from secret yml
-app.secret_key = b'_1#y3L"Q4T8z\n\xbc]/'
-
-TODOS = {
-    'todo1': {'task': 'build an API'},
-    'todo2': {'task': '?????'},
-    'todo3': {'task': 'profit!'},
-}
+@jwt.unauthorized_loader
+def unauthorized_callback(expired_token):
+    return jsonify({
+        'status': 401,
+        'sub_status': 42,
+        'msg': 'No authorization token provided'
+    }), 401
 
 
-@auth.verify_password
-def verify_password(username_or_token, password):
-    """Verify a username:password or token."""
-    # first try to authenticate by token
-    user = User.verify_auth_token(username_or_token,
-                                  app.config['SECRET_KEY'])
-    if not user:
-        # try to authenticate with username/password
-        try:
-            user = User.objects.get(username=username_or_token)
-        except DoesNotExist:
-            return False
-        if not user.password == password:
-            return False
-    g.user = user
-    return True
+@app.route('/token', methods=['POST'])
+def login():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    if not username:
+        return jsonify({"msg": "Missing username parameter"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password parameter"}), 400
+
+    if '$' in username:     # TODO scrub username input better
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    user = User.find_user_by_username(username)
+
+    if not user or not password == user.password:
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    # Identity can be any data that is json serializable
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token), 200
 
 
-class Token(Resource):
-    """Allow a user to request a token for use instead of username:password."""
-
-    @auth.login_required
-    def get(self):
-        """Return a new token for an authenticated user."""
-        token = g.user.generate_auth_token(app.config['SECRET_KEY'], 600)
-        return jsonify({'token': token.decode('ascii'), 'duration': 600})
+@app.route('/whoami', methods=['GET'])
+@jwt_required
+def whoami():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
 
 
-class Todo(Resource):
-    """Handle a single Todo items."""
-
-    def _abort_if_todo_doesnt_exist(self, todo_id):
-        if todo_id not in TODOS:
-            abort(404, message="Todo {} doesn't exist".format(todo_id))
-
-    def get(self, todo_id):
-        """Return a ToDo by id."""
-        self._abort_if_todo_doesnt_exist(todo_id)
-        return TODOS[todo_id]
-
-    @auth.login_required
-    def delete(self, todo_id):
-        """Delete a ToDo by id."""
-        self._abort_if_todo_doesnt_exist(todo_id)
-        del TODOS[todo_id]
-        return '', 204
-
-    @auth.login_required
-    def put(self, todo_id):
-        """Create or update a ToDo."""
-        args = parser.parse_args()
-        task = {'task': args['task']}
-        TODOS[todo_id] = task
-        return task, 201
+def graphql_token_view():
+    view = GraphQLView.as_view('graphql',
+                               schema=schema,
+                               graphiql=bool(app.config.get("DEBUG", False)))
+    view = jwt_required(view)  # TODO doesn't fail nicely when no JWT
+    return view
 
 
-# TodoList
-# shows a list of all todos, and lets you POST to add new tasks
-class TodoList(Resource):
-    """Handle lists of Todo items."""
-
-    def get(self):
-        """Return the list of all Todos."""
-        return TODOS
-
-    @auth.login_required
-    def post(self):
-        """Add multiple Todo items at once."""
-        args = parser.parse_args()
-        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
-        todo_id = 'todo%i' % todo_id
-        TODOS[todo_id] = {'task': args['task']}
-        return TODOS[todo_id], 201
+app.add_url_rule('/graphql', view_func=graphql_token_view)
 
 
-# Add routes to the API resources
-api.add_resource(TodoList, '/todos')
-api.add_resource(Todo, '/todos/<todo_id>')
-api.add_resource(Token, '/token')
+def load_secret(filename='/run/secrets/flask.key'):
+    """Load a secret key from file."""
+    print(f'Reading secret key from {filename}')
+    with open(filename, 'r') as stream:
+        key = stream.read()
+    app.secret_key = key
 
 
 def main():
     """Start the application."""
     # import IPython; IPython.embed()  # noqa: E702 <<< BREAKPOINT >>>
+    load_secret()
     connect_from_config()
+
+    # read and set secret
     app.run(host='0.0.0.0', debug=True)
 
 
