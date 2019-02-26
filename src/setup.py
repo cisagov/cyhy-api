29c62dfa99cebd420054ac04147821800487f70f
Felddy <markf+github@geekpad.com>
2019-02-26 15:52:29 -0500

    Oww my quotes.

--- src/setup.py
+++ src/setup.py
@@ -6,6 +6,7 @@ install_requires = [
     "Flask == 1.0.2",
     "Flask-JWT-Extended == 3.17.0",
     "Flask-GraphQL == 2.0.0",
+    "Flask-GraphQL-Auth == 1.1.2",
     "graphene-mongo == 0.2.0",
     "mongoengine == 0.16.3",
     "Flask-Bcrypt",  # compiled in Dockerfile
@@ -14,27 +15,19 @@ install_requires = [
     "python-dateutil == 2.7.5",
 ]
 
-tests_require = [
-    'pytest == 4.1.1',
-    'mock == 2.0.0',
-    'mongomock == 3.15.0'
-]
+tests_require = ["pytest == 4.1.1", "mock == 2.0.0", "mongomock == 3.15.0"]
 
 setup(
-    name='cyhy_api',
-    version='0.0.1',
-    author='Mark Feldhousen',
-    author_email='mark.feldhousen@trio.dhs.gov',
+    name="cyhy_api",
+    version="0.0.1",
+    author="Mark Feldhousen",
+    author_email="mark.feldhousen@trio.dhs.gov",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    entry_points={
-        'console_scripts': [
-            'cyhy-api-server=cyhy_api.api:main',
-        ],
-    },
-    license='LICENSE.txt',
-    description='Cyber Hygiene API Server',
+    entry_points={"console_scripts": ["cyhy-api-server=cyhy_api.api:main"]},
+    license="LICENSE.txt",
+    description="Cyber Hygiene API Server",
     # long_description=open('README.md').read(),
     install_requires=install_requires + tests_require,
     # tests_require=tests_require
