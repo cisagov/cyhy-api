f1374ba100b377c6080fa1b71ab3b1ea2843f62d
Felddy <markf+github@geekpad.com>
2019-02-26 23:12:03 -0500

    Remove unused id parameter

--- src/cyhy_api/schema/queries/user.py
+++ src/cyhy_api/schema/queries/user.py
@@ -7,10 +7,9 @@ from flask_graphql_auth import query_jwt_required
 
 @query_jwt_required
 def resolve_user(root, info, **kwargs):
-    id = kwargs.get("id", None)
     username = kwargs.get("username", None)
 
-    users = UserModel.objects(id=id, username=username)
+    users = UserModel.objects(username=username)
 
     if users.first() is None:
         return ResponseMessageField(is_success=False, message="Not found")
