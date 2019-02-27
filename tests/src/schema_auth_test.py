915cea027ef786a7c420d2cb2bd7aecc45187ce7
Felddy <markf+github@geekpad.com>
2019-02-26 23:15:16 -0500

    Add test for GraphQL authentication

---
+++ tests/src/schema_auth_test.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env pytest -vs
+"""Tests for GraphQL authentication API."""
+
+import pytest
+from graphene.test import Client
+from cyhy_api.schema import schema
+from mongoengine import connect
+from cyhy_api.model import UserModel
+from cyhy_api.api import create_app
+
+
+@pytest.fixture(scope="class", autouse=True)
+def connection():
+    """Create connections for tests to use."""
+    connect(host="mongomock://localhost", alias="default")
+
+
+@pytest.fixture(scope="class", autouse=True)
+def context():
+    """Create the Flask application for use with contexts."""
+    app = create_app()
+    return app.app_context()
+
+
+@pytest.fixture(scope="class")
+def client():
+    """Create a client."""
+    return Client(schema)
+
+
+class TestAuth:
+    """GraphQL authentication tests."""
+
+    def test_register_mutation(self, client):
+        """Test user registration."""
+        query = """
+                mutation {
+                  register(username: "bobbo", password: "foobar") {
+                    result {
+                      isSuccess
+                      message
+                    }
+                  }
+                }
+                """
+        executed = client.execute(query)
+        # the api says it worked
+        assert (
+            executed["data"]["register"]["result"]["isSuccess"] is True
+        ), "Expected success."
+        # let's check the database and see if the user is really there
+        user = UserModel.objects(username="bobbo").first()
+        assert user is not None
+        assert user.username == "bobbo"
+        assert user.password == "foobar"
+
+    def test_auth_mutation(self, client, context):
+        """Test user authentication."""
+        query = """
+                mutation {
+                  auth(username: "bobbo", password: "foobar") {
+                    result {
+                      __typename
+                      ... on AuthField {
+                        accessToken
+                        refreshToken
+                        message
+                      }
+                      ... on ResponseMessageField {
+                        message
+                        isSuccess
+                      }
+                    }
+                  }
+                }
+                """
+        with context:
+            executed = client.execute(query)
+        print(executed)
+        assert (
+            executed["data"]["auth"]["result"]["__typename"] == "AuthField"
+        ), "Expected an AuthField response."
+        assert (
+            executed["data"]["auth"]["result"]["accessToken"] is not None
+        ), "Expected an accessToken."
+        assert (
+            executed["data"]["auth"]["result"]["refreshToken"] is not None
+        ), "Expected an refreshToken."
+
+        # TODO use the tokens
