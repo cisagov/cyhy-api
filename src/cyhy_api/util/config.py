a08f0b1a4cd861bacb3365e72d4f31418e27dc1b
Felddy <markf+github@geekpad.com>
2019-04-22 16:37:24 -0400

    Add ability to assign any connection alias as the default

--- src/cyhy_api/util/config.py
+++ src/cyhy_api/util/config.py
@@ -8,14 +8,24 @@ def load_config(filename="/run/secrets/config.yml"):
     """Load a configuration file."""
     print(f"Reading configuration from {filename}")
     with open(filename, "r") as stream:
-        config = yaml.load(stream)
+        config = yaml.safe_load(stream)
     return config
 
 
-def connect_from_config(config=None):
-    """Create connections from a confguration."""
+def connect_from_config(config=None, default_alias=None):
+    """Create connections from a confguration.
+
+    config: configuration file to load
+    default_alias: if set, a single connection is set as the default.
+                   if unset, all aliases in the config will be created.
+    """
     if not config:
         config = load_config()
     connections = config["connections"]
-    for alias in connections.keys():
-        connect(host=connections[alias]["uri"], alias=alias)
+    if default_alias is not None:
+        # only create the specified connection and map it to the "default" alias
+        connect(host=connections[default_alias]["uri"], alias="default")
+    else:
+        # create all connections
+        for alias in connections.keys():
+            connect(host=connections[alias]["uri"], alias=alias)
